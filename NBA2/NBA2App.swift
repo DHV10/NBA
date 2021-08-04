5cc121c90c91280bdf8efb11a6bf5520aa975b0d
DHV10 <supervietfs@gmail.com>
2021-08-04 14:57:05 +0700

    Initial Commit

---
+++ NBA2/NBA2App.swift
@@ -0,0 +1,17 @@
+//
+//  NBA2App.swift
+//  NBA2
+//
+//  Created by DHV on 04/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct NBA2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
